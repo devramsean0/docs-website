9f95309725743716cd8f4b9262fc28da1ba7509a
Sean Outram <outramsean0@gmail.com>
2023-11-29 12:40:11 +0000

    Update submodule, and work on package selection

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export function Footer() {
+    return (
+        <section id="footer">
+                <h2>&copy; Sean Outram and individual package contributors 2023</h2>
+                <p>
+                    All linked documentation is under the MIT License unless explicitly specified
+                </p>
+                <span className="text-blue-300"><Link href="https://github.com/devramsean0">GitHub</Link> | <Link href="https://sean.cyou">Main Site</Link></span>
+        </section>
+    )
+}
